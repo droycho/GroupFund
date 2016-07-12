1090f307de621dbec32bd4b5e2ebd8571fbe2b57
DJ Roychoudhury <djroychoudhury@yahoo.com>
2016-07-12 13:30:03 -0700

    added intent to MainActivity to go to HomeActivity.

--- app/src/main/java/com/epicodus/groupfund/ui/MainActivity.java
+++ app/src/main/java/com/epicodus/groupfund/ui/MainActivity.java
@@ -1,15 +1,27 @@
 package com.epicodus.groupfund.ui;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.epicodus.groupfund.R;
 
 public class MainActivity extends AppCompatActivity {
+    private Button mLoginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mLoginButton = (Button) findViewById(R.id.loginButton);
+        mLoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/epicodus/groupfund/ui/NewEventActivity.java
+++ app/src/main/java/com/epicodus/groupfund/ui/NewEventActivity.java
@@ -5,11 +5,11 @@ import android.os.Bundle;
 
 import com.epicodus.groupfund.R;
 
-public class HomeActivity extends AppCompatActivity {
+public class NewEventActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.activity_new_event);
     }
 }

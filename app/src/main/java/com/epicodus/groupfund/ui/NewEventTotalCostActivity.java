2778d00dca951bb901b01f87bfd13722e345e31f
DJ Roychoudhury <djroychoudhury@yahoo.com>
2016-07-17 23:39:09 -0700

    correctly logs total cost.

--- app/src/main/java/com/epicodus/groupfund/ui/NewEventTotalCostActivity.java
+++ app/src/main/java/com/epicodus/groupfund/ui/NewEventTotalCostActivity.java
@@ -3,23 +3,31 @@ package com.epicodus.groupfund.ui;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import com.epicodus.groupfund.R;
 
 public class NewEventTotalCostActivity extends AppCompatActivity {
+    public static final String TAG = NewEventTotalCostActivity.class.getSimpleName();
     private Button mNewTotalCostToInviteButton;
+    private EditText mNewTotalCostEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_event_total_cost);
+//        NewEventTotalCostInputField
+        mNewTotalCostEditText = (EditText) findViewById(R.id.newTotalCostEditText);
 //        InviteButton
         mNewTotalCostToInviteButton = (Button) findViewById(R.id.inviteButton);
         mNewTotalCostToInviteButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                String totalCost = mNewTotalCostEditText.getText().toString();
+                Log.d(TAG, totalCost);
                 Intent intent = new Intent(NewEventTotalCostActivity.this, InviteActivity.class);
                 startActivity(intent);
             }

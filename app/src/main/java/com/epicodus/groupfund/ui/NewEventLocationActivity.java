afdf41dea217ef29cc35246f19fa33eec1da77d5
DJ Roychoudhury <djroychoudhury@yahoo.com>
2016-07-17 23:30:14 -0700

    correctly logs location.

--- app/src/main/java/com/epicodus/groupfund/ui/NewEventLocationActivity.java
+++ app/src/main/java/com/epicodus/groupfund/ui/NewEventLocationActivity.java
@@ -3,23 +3,31 @@ package com.epicodus.groupfund.ui;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import com.epicodus.groupfund.R;
 
 public class NewEventLocationActivity extends AppCompatActivity {
+    public static final String TAG = NewEventLocationActivity.class.getSimpleName();
     private Button mNewLocationToNewDescriptionButton;
+    private EditText mNewLocationEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_event_location);
+//        NewEventLocationInputField
+        mNewLocationEditText = (EditText) findViewById(R.id.newLocationEditText);
 //        NextStepButton
         mNewLocationToNewDescriptionButton = (Button) findViewById(R.id.newLocationToNewDescriptionButton);
         mNewLocationToNewDescriptionButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                String location = mNewLocationEditText.getText().toString();
+                Log.d(TAG, location);
                 Intent intent = new Intent(NewEventLocationActivity.this, NewEventDescriptionActivity.class);
                 startActivity(intent);
             }

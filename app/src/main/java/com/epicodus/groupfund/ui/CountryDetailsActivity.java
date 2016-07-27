cf9e0c76f929d515eb99de82569f4b0080b78a3e
DJ Roychoudhury <djroychoudhury@yahoo.com>
2016-07-27 09:42:51 -0700

    fixed issue with README getting deleted.

--- app/src/main/java/com/epicodus/groupfund/ui/CountryDetailsActivity.java
+++ app/src/main/java/com/epicodus/groupfund/ui/CountryDetailsActivity.java
@@ -1,16 +1,51 @@
 package com.epicodus.groupfund.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 import com.epicodus.groupfund.R;
+import com.epicodus.groupfund.services.TravelbriefingService;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Response;
 
 public class CountryDetailsActivity extends AppCompatActivity {
+    public static final String TAG = CountryActivity.class.getSimpleName();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_country_details);
 
+        Intent intent = getIntent();
+        String country = intent.getStringExtra("country");
+
+        getCountry(country);
+    }
+
+    private void getCountry(String country) {
+        final TravelbriefingService travelbriefingService = new TravelbriefingService();
+        travelbriefingService.findCountries(country, new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                try{
+                    String jsonData = response.body().string();
+                    Log.v(TAG, jsonData);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 }

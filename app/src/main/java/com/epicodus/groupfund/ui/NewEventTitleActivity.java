fb66a02a8449c2374f4a59d0de8ad0f490443dce
DJ Roychoudhury <djroychoudhury@yahoo.com>
2016-07-26 20:55:23 -0700

    eventListener for firebase data logs all elements of each node, instead of jsut the title. Not sure if this is supposed to happen or not because class example only uses one parameter, i'm using 6.

--- app/src/main/java/com/epicodus/groupfund/ui/NewEventTitleActivity.java
+++ app/src/main/java/com/epicodus/groupfund/ui/NewEventTitleActivity.java
@@ -3,6 +3,7 @@ package com.epicodus.groupfund.ui;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -10,8 +11,11 @@ import android.widget.EditText;
 import com.epicodus.groupfund.Constants;
 import com.epicodus.groupfund.R;
 import com.epicodus.groupfund.models.Event;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -37,6 +41,21 @@ public class NewEventTitleActivity extends AppCompatActivity implements View.OnC
                 .getInstance()
                 .getReference()
                 .child(Constants.FIREBASE_CHILD_EVENT);
+
+        mEventReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for (DataSnapshot titleSnapshot : dataSnapshot.getChildren()) {
+                    String title = titleSnapshot.getValue().toString();
+                    Log.d("Titles updated", "title: " + title);
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_event_title);
